433594d780e196c063c596c7d200dabbc20cac92
AleHts29 <ahuertas@fi.uba.ar>
2021-11-16 00:51:33 -0300

    Express Avansado

--- Clase_06/CRUD/index.js
+++ Clase_06/CRUD/index.js
@@ -3,6 +3,9 @@ const express = require("express");
 const app = express();
 const arr = require('./arr')
 
+// Le informo a mi app que va a reciboir archivos en formato JSON
+app.use(express.json());
+
 console.log(arr)
 
 
@@ -49,7 +52,60 @@ app.get('/:id', (req, res) => {
 
 
 
-// POST
+// POST --> Envio datos para que el server los almacene
+// Envio informacion mediante POST usando Thunder Client o postMan
+app.post('/', (req, res) =>{
+    console.log(req.body)
+
+    const obj = {
+        name: req.body.name,
+        price: req.body.price,
+        id: arr.length + 1
+    };
+    arr.push(obj);
+
+    res.send('Se agrego un nuevo producto de forma exitosa!..')
+})
+
+
+// PUT --> Actualiza determinados datos del lado del server
+app.put('/:id', (req, res) => {
+
+    // me muestra lo que viene de la url --> en este ejemplo el id
+    console.log(req.params);
+
+    // me muestra los datos enviados por put, que son los que quiero asignar en el id que envio por la url
+    console.log(req.body);
+
+    // desestructuracion --> ID que viene de la URL
+    let {id} = req.params;
+
+    // desestructuracion --> Datos que vienen de Thunder Client o postMan (metodo PUT)
+    let {name, price} = req.body;
+
+    const newArr = arr.filter((a) => {
+        return a.id == parseInt(id);
+    })
+
+    for (const key in newArr) {
+        newArr[key].name = name;
+        newArr[key].price = price;
+    }
+        
+    res.send(`El id:${id} se actualizo correctamente`);
+
+})
+
+
+// DELETE
+app.delete('/:id', (req, res) => {
+    let {id} = req.params;
+
+    // Falta logica
+
+    res.send(`El id:${id} se elimono correctamente`)
+})
+
 
 app.listen(3002, () => {
     console.log('server run on port 3002');
