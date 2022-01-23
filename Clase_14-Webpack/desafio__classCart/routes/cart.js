a275b38440553103c88cbeb98964f9caa6ef90cd
AleHts29 <ahuertas@fi.uba.ar>
2022-01-23 19:01:32 -0300

    Se desarrolla modulo cart y classCart

--- Clase_14-Webpack/desafio__classCart/routes/cart.js
+++ Clase_14-Webpack/desafio__classCart/routes/cart.js
@@ -1,16 +1,68 @@
+// Para este último caso incorporar los botones actualizar y eliminar.
+// También tendremos un formulario de ingreso de productos nuevos con los campos
+// correspondientes y un botón enviar. Asimismo, construir la vista del carrito donde se
+// podrán ver los productos agregados e incorporar productos a comprar por su id de
+// producto. Esta aplicación de frontend debe enviar los requests get, post, put y delete al
+// servidor utilizando fetch y debe estar ofrecida en su espacio público.
+
 const express = require("express");
-const ClassContainer = require("../src/utils/container.js");
+const moment = require("moment");
+
+const ClassContainer = require("../src/utils/classCart.js");
 
 const { Router } = express;
 const router = new Router();
-const Container = new ClassContainer("./src/data/products.txt");
+const Container = new ClassContainer("./src/data/cart.txt");
 
 router.use(express.json());
 router.use(express.urlencoded({ extended: false }));
 
 // Logica
 router.get("/", async (req, res) => {
-  res.send("Soy el carrito");
+  let dataFIle = await Container.getAll();
+
+  res.send("Soy el GET");
+});
+
+router.get("/:id/products", async (req, res) => {
+  let { id } = req.params;
+  let dataFIle = await Container.getAllByCart(id);
+  console.log(dataFIle);
+
+  res.send("Soy el GET");
+});
+
+router.post("/", async (req, res) => {
+  // Fecha
+  let date = moment().format("DD/MM/YYYY hh:mm:ss");
+
+  // se agrega el nuevo elemento al archivo.txt
+  let newCart = await Container.newCart(date);
+  console.log(newCart);
+  res.send("Soy el POST");
+});
+
+router.post("/:id/product", async (req, res) => {
+  let { id } = req.params;
+  let newProduct = req.body;
+
+  let newProductCart = await Container.save(id, newProduct);
+  // Se muestra renderizado el producto buscado
+  res.send("Soy el POST:id/producto");
+});
+
+router.delete("/:id", async (req, res) => {
+  let { id } = req.params;
+  let deleteCart = await Container.deleteById(id);
+  console.log(deleteCart);
+  res.send("Soy DELETE");
+});
+
+router.delete("/:id/products/:id_prod", async (req, res) => {
+  let { id, id_prod } = req.params;
+  let deleteCart = await Container.deleteByIdProduct(id, id_prod);
+  console.log(deleteCart);
+  res.send("Soy DELETE");
 });
 
 module.exports = router;
